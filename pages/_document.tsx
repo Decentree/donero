f18e692861f5b3a0ad098d59eb85183d11429d48
AdamSchinzel <schinzel.adam@gmail.com>
2022-12-15 06:28:57 +0100

    add description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         <title>Donero</title>
+        <meta name="description" content="Donero is a donation platform that allows you to donate to your favorite charities and causes with crypto."/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"/>
